d2f7a35f3c6782154ab5650114302ac7f28ad92e
Anisur <anisurtm2@gmail.com>
2024-01-20 21:11:50 +0600

    first commit

---
+++ jqueye.js
@@ -0,0 +1,14 @@
+function showHide() {
+    var valueMc = document.getElementById("div7");
+    if (valueMc.style.display === "none") {
+        valueMc.style.display = "block";
+    } else {
+      valueMc.style.display = "none";
+    }
+  }
+
+
+
+// function showHide() {
+//     document.getElementById('div7').style.display = "block";
+//  }
